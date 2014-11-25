8250d8703d36e33ec84d89e500b1c19623048eb0
Soumen <souman.trivedi@ordnancesurvey.co.uk>
2014-11-25 11:31:16 +0000

    changed the lts & repo flag to be proper boolean value based upon stable stdlib functions used by rtyler/jenkins

--- manifests/init.pp
+++ manifests/init.pp
@@ -21,77 +21,109 @@
 #
 
 class puppetci {
+  package { 'java-1.7.0-openjdk': ensure => installed, }
 
-  package { 'java-1.7.0-openjdk':
-    ensure => installed,
-  }
-
-
-
-  #For displaying fonts
-  package{
-    'dejavu-lgc-sans-mono-fonts':;
-    'dejavu-sans-mono-fonts':;
-  }
-
-  #for Git :)
+  # For displaying fonts
   package {
-    'git':;
+    'dejavu-lgc-sans-mono-fonts':
+    ;
+
+    'dejavu-sans-mono-fonts':
+    ;
   }
 
-  #For RVM 
+  # for Git :)
+  package { 'git': ; }
+
+  # For RVM
   package {
-    'gcc-c++':;
-    'patch':;
-    'readline':;
-    'readline-devel':;
-    'zlib':;
-    'zlib-devel':;
-    'libyaml-devel':;
-    'libffi-devel':;
-    'openssl-devel':;
-    'libxml2-devel':;
-    'libxslt-devel':;
-    'make':;
-    'bzip2':;
-    'autoconf':;
-    'automake':;
-    'libtool':;
-    'bison':;
-    'libcurl-devel':;
+    'gcc-c++':
+    ;
+
+    'patch':
+    ;
+
+    'readline':
+    ;
+
+    'readline-devel':
+    ;
+
+    'zlib':
+    ;
+
+    'zlib-devel':
+    ;
+
+    'libyaml-devel':
+    ;
+
+    'libffi-devel':
+    ;
+
+    'openssl-devel':
+    ;
+
+    'libxml2-devel':
+    ;
+
+    'libxslt-devel':
+    ;
+
+    'make':
+    ;
+
+    'bzip2':
+    ;
+
+    'autoconf':
+    ;
+
+    'automake':
+    ;
+
+    'libtool':
+    ;
+
+    'bison':
+    ;
+
+    'libcurl-devel':
+    ;
   }
 
-  class {'jenkins':
-    lts  => 0,
-    repo => 1,
+  class { 'jenkins':
+    lts  => true,
+    repo => true,
   }
 
-  class {'puppetci::plugins':
+  class { 'puppetci::plugins':
     require => Package['jenkins'];
   }
+
   file {
     '/var/lib/jenkins/org.jenkinsci.plugins.ghprb.GhprbTrigger.xml':
-    source => 'puppet:///modules/puppetci/org.jenkinsci.plugins.ghprb.GhprbTrigger.xml',
-    replace => 'no', 
-    owner => 'jenkins',
-    group => 'jenkins';
+      source  => 'puppet:///modules/puppetci/org.jenkinsci.plugins.ghprb.GhprbTrigger.xml',
+      replace => 'no',
+      owner   => 'jenkins',
+      group   => 'jenkins';
 
     '/var/lib/jenkins/jobs':
       ensure => directory,
-      owner => 'jenkins',
-      group => 'jenkins';
+      owner  => 'jenkins',
+      group  => 'jenkins';
 
     '/var/lib/jenkins/jobs/PuppetCI':
       ensure => directory,
-      owner => 'jenkins',
-      group => 'jenkins';
+      owner  => 'jenkins',
+      group  => 'jenkins';
 
     '/var/lib/jenkins/jobs/PuppetCI/config.xml':
-      ensure => file,
-      replace => 'no', 
-      source => 'puppet:///modules/puppetci/config.xml',
-      owner => 'jenkins',
-      group => 'jenkins';
+      ensure  => file,
+      replace => 'no',
+      source  => 'puppet:///modules/puppetci/config.xml',
+      owner   => 'jenkins',
+      group   => 'jenkins';
   }
 
 }
